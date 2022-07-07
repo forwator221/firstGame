042d8300918eb2509b78cf9d699ae245de4f400c
Dinis <forwator221@gmail.com>
2022-07-08 02:02:26 +0500

    Add Weapon Manager Script, weapons scriptoble object and item class. Add WatermelonSword

---
+++ Assets/WeaponSlotManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponSlotManager : MonoBehaviour
+{
+    WeaponHolderSlot leftHandSlot;
+    WeaponHolderSlot rightHandSlot;
+
+    private void Awake()
+    {
+        WeaponHolderSlot[] weaponHolderSlots = GetComponentsInChildren<WeaponHolderSlot>();
+        foreach (WeaponHolderSlot weaponSlot in weaponHolderSlots)
+        {
+            if (weaponSlot.isLeftHandSlot)
+            {
+                leftHandSlot = weaponSlot;
+            }
+            else if (weaponSlot.isRightHandSlot)
+            {
+                rightHandSlot = weaponSlot;
+            }
+        }
+    }
+
+    public void LoadOnWeaponSlot(WeaponItem weaponItem, bool isLeft)
+    {
+        if (isLeft)
+        {
+            leftHandSlot.LoadWeponModel(weaponItem);
+        }
+        else
+        {
+            rightHandSlot.LoadWeponModel(weaponItem);
+        }
+    }
+}
